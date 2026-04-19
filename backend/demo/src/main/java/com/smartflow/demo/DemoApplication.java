9550a6eb35a12bcbb0b1d15fcb588c80606f3a33
Tanmay Singh <tanmaysingh1208@gmail.com>
2026-04-19 10:28:47 +0530

    fix: hardcode DB pool limits in DemoApplication to fix HF crash

--- backend/demo/src/main/java/com/smartflow/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/smartflow/demo/DemoApplication.java
@@ -2,12 +2,21 @@ package com.smartflow.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Properties;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication app = new SpringApplication(DemoApplication.class);
+		
+		// Set default pool limits to prevent connection exhaustion on constrained environments (like HF)
+		Properties props = new Properties();
+		props.put("spring.datasource.hikari.maximum-pool-size", "3");
+		props.put("spring.datasource.hikari.minimum-idle", "1");
+		app.setDefaultProperties(props);
+		
+		app.run(args);
 	}
 
 }
