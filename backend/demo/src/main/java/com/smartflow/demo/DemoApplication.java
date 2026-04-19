fb38a2344b7d5215f94bca77a11d0c72bacf62fe
Tanmay Singh <tanmaysingh1208@gmail.com>
2026-04-19 10:31:07 +0530

    fix: hardcode google clientId in DemoApplication to ensure auth works on HF

--- backend/demo/src/main/java/com/smartflow/demo/DemoApplication.java
+++ backend/demo/src/main/java/com/smartflow/demo/DemoApplication.java
@@ -10,10 +10,11 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(DemoApplication.class);
 		
-		// Set default pool limits to prevent connection exhaustion on constrained environments (like HF)
+		// Set default properties to ensure stability and feature-completeness on constrained environments (like HF)
 		Properties props = new Properties();
 		props.put("spring.datasource.hikari.maximum-pool-size", "3");
 		props.put("spring.datasource.hikari.minimum-idle", "1");
+		props.put("google.client.id", "800791620013-uj3mtbfo91cb6isaru29mgr6k2rbr3lo.apps.googleusercontent.com");
 		app.setDefaultProperties(props);
 		
 		app.run(args);
